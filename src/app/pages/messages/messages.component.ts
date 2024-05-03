8feba395db448c79cd034b4da73a640b4da7b6cb
EQuarcoo <get>
2024-05-03 17:12:17 +0000

    create Login and Registration components

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CardComponent } from '../../shared/component/card/card.component';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
